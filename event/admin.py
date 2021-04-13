880cc3e53a0fdebaa3ce9db603c0a893f6315bbc
Vikas Bahuguna <vikasedu10@gmail.com>
2021-04-13 12:23:52 +0530

    Add event models & register

--- event/admin.py
+++ event/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from event.models import Event
+
+
+@admin.register(Event)
+class EventsInstanceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'place_location', 'updated_at', 'created_by')
--- event/models.py
+++ event/models.py
@@ -1,3 +1,28 @@
-from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.gis.db import models
+# from django.db import models
+from taggit.managers import TaggableManager
 
-# Create your models here.
+
+class PlaceType(models.Model):
+    place_type = models.CharField(max_length=200, help_text="Enter place type")
+
+    def __str__(self):
+        return self.place_type
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    place_location = models.PointField(blank=True, null=True)
+    tags = TaggableManager()
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def lat_lon(self):
+        return list(getattr(self.location, 'coords', [])[::-1])
