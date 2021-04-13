674682b6f39f22b90011fa3bdb9577c966eaaa72
Vikas Bahuguna <vikasedu10@gmail.com>
2021-04-13 12:24:54 +0530

    Add view function, add path to view

--- event/urls.py
+++ event/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from event import views
 
+from event.views import EventsListView
+
 
 urlpatterns = [
     path('', views.index, name="index"),
+
+    path('all/', EventsListView.as_view(), name="events"),
 ]
--- event/views.py
+++ event/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+
+from event.models import Event
 
 
 def index(request):
     return render(request, 'events/index.html')
+
+
+class EventsListView(generic.ListView):
+    model = Event
+    template_name = 'events/events.html'
+    context_object_name = 'events'
