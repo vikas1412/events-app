7096beef6b603c72c2451dbab3dcb069ff636297
Vikas Bahuguna <vikasedu10@gmail.com>
2021-04-14 11:24:46 +0530

    Register All models

--- event/admin.py
+++ event/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
+from django.contrib.gis.admin import OSMGeoAdmin
 
-from event.models import Event, PlaceType
+from event.models import Event, PlaceType, Place
 
-admin.site.register('PlaceType',)
+admin.site.register(PlaceType)
 
 
 @admin.register(Event)
-class EventsInstanceAdmin(admin.ModelAdmin):
+class EventsInstanceAdmin(OSMGeoAdmin):
+    default_lon = 30.287842565701155
+    default_lat = 78.10559462066462
+    default_zoom = 12
     list_display = ('title', 'updated_at', 'created_by')
+
+
+@admin.register(Place)
+class PlaceInstanceAdmin(OSMGeoAdmin):
+    default_lon = 30.287842565701155
+    default_lat = 78.10559462066462
+    default_zoom = 12
+    list_display = ('title', 'city', 'type_of_place', 'timestamp')
