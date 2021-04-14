baf257bf8abe30f92f37c3d39bef0f1a0cf44bed
Vikas Bahuguna <vikasedu10@gmail.com>
2021-04-14 11:14:07 +0530

    Add paths to NewPlaceCreateView, NewPlaceDetailCreateView

--- event/urls.py
+++ event/urls.py
@@ -5,7 +5,13 @@ from event.views import EventsListView
 
 urlpatterns = [
     path('', views.index, name="index"),
-    
+
+    path('all/', EventsListView.as_view(), name="events"),
+
+    path('add/place-type/', views.NewPlaceCreateView.as_view(), name="new-place"),
+
+    path('add/place/', views.NewPlaceDetailCreateView.as_view(), name="new-place-detail"),
+
 
     path('places/', views.PlacesListView.as_view(), name="places"),
 ]
