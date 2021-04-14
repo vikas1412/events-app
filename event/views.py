ff4acb1f33220bdcba91312663bbafb018b895c8
Vikas Bahuguna <vikasedu10@gmail.com>
2021-04-14 11:09:47 +0530

    Add PlaceListView

--- event/views.py
+++ event/views.py
@@ -1,14 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.views import generic
 
-from event.models import Event
+from event.forms import SignUpForm
+from event.models import Event, Place, PlaceType
 
 
 def index(request):
     return render(request, 'events/index.html')
 
 
-class EventsListView(generic.ListView):
-    model = Event
-    template_name = 'events/events.html'
-    context_object_name = 'events'
+class PlacesListView(generic.ListView):
+    model = Place
+    template_name = 'events/places.html'
+    context_object_name = 'places'
