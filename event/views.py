dd2372851412c0f433dfb37ce2035371331263cf
Vikas Bahuguna <vikasedu10@gmail.com>
2021-04-15 10:39:25 +0530

    Save more fields to db, add filter_events func

--- event/views.py
+++ event/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views import generic
 
+from event.filters import EventFilter
 from event.forms import SignUpForm
 from event.models import Event, Place, PlaceType
 
@@ -22,15 +25,31 @@ class EventsDetailView(generic.DetailView):
     template_name = 'events/event.html'
     context_object_name = 'event'
 
+    def get_context_data(self, **kwargs):
+        context = super(EventsDetailView, self).get_context_data(**kwargs)
+        return context
 
-class NewEventCreateView(generic.CreateView):
+
+class NewEventCreateView(LoginRequiredMixin, generic.CreateView):
     model = Event
     template_name = 'events/new-event.html'
-    fields = ['title', 'place', 'type_of_place', 'tags', 'description', 'time_start', 'time_end']
+    fields = ['title', 'place', 'type_of_place', 'date', 'tags', 'description', 'time_start', 'time_end']
     context_object_name = 'new_event'
 
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
 
-class NewPlaceCreateView(generic.CreateView):
+        year, month, day = str(form.instance.date).split('-')
+        print(year, '\n', month, '\n', day)
+
+        form.instance.year = int(year)
+        form.instance.month = int(month)
+        form.instance.day = int(day)
+        form.instance.all_day = True
+        return super(NewEventCreateView, self).form_valid(form)
+
+
+class NewPlaceCreateView(LoginRequiredMixin, generic.CreateView):
     model = PlaceType
     template_name = 'events/new-place.html'
     fields = ['place_type']
@@ -38,7 +57,7 @@ class NewPlaceCreateView(generic.CreateView):
     success_url = '/events/places/'
 
 
-class NewPlaceDetailCreateView(generic.CreateView):
+class NewPlaceDetailCreateView(LoginRequiredMixin, generic.CreateView):
     model = Place
     template_name = 'events/new-place-detail.html'
     fields = ['title', 'type_of_place', 'tags', 'location', 'description', 'address', 'phone', 'city']
@@ -69,3 +88,17 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
+
+
+def filter_events(request):
+
+    events = Event.objects.all()
+    print(events)
+    event_filter = EventFilter(request.GET, queryset=events)
+    events = event_filter.qs
+ 
+    params = {
+        'filter': event_filter,
+        'events': events,
+    }
+    return render(request, 'events/filter.html', params)
